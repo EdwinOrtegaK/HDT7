4b0d502c806d7ee6aecaf4de8fa403ba547f6561
EdwinOrtegaK <andyortegakou@yahoo.com.mx>
2023-03-25 16:27:04 -0600

    First Commit

---
+++ src/Reader.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Reader {
+    ArrayList<String> Keys = new ArrayList<String>();
+    public static ArrayList<String> leer() {
+        ArrayList<String> lineas = new ArrayList<>();
+
+        try {
+            File archivo = new File("diccionario.txt");
+            Scanner scanner = new Scanner(archivo);
+
+            while (scanner.hasNextLine()) {
+                String linea = scanner.nextLine();
+                lineas.add(linea);
+            }
+            scanner.close();
+
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error: " + e.getMessage());
+        }
+        return lineas;
+    }
+}
